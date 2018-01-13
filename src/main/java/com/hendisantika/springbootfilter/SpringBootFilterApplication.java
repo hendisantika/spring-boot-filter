0ab850e73a03c908154b93867da4213c735388d7
Hendi Santika <hendisantika@gmail.com>
2018-01-13 09:02:21 +0700

    #11 - Modify Main App

--- src/main/java/com/hendisantika/springbootfilter/SpringBootFilterApplication.java
+++ src/main/java/com/hendisantika/springbootfilter/SpringBootFilterApplication.java
@@ -1,12 +1,28 @@
 package com.hendisantika.springbootfilter;
 
+import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class SpringBootFilterApplication {
+@ComponentScan(basePackages = "com.hendisantika")
+@EnableAutoConfiguration
+public class SpringBootFilterApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFilterApplication.class, args);
 	}
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new SpecificationArgumentResolver());
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
+    }
 }
