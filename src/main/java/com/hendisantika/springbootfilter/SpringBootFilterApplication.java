1b06fa920dfb580bd40538f5b7682aae605a18c8
Hendi Santika <hendisantika@gmail.com>
2018-01-13 07:59:56 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootfilter/SpringBootFilterApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFilterApplication.class, args);
+	}
+}
